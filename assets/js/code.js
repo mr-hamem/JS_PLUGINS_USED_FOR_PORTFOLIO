3b0bbec515e501d0546d9e939eb296f518b5ad64
MR HAMEM <hamemapple2018@gmail.com>
2025-12-28 03:59:45 +0600

    added typed script, sweetalert

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,3 +1,4 @@
+// windows smooth scroll js start here
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   const bannerHeight = document.querySelector("#banner").offsetHeight;
@@ -8,3 +9,33 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+// windows smooth scroll js ends here
+// slick slider js starts here
+$(document).ready(function () {
+  $('.your-class').slick({
+    slidesToShow: 2, 
+    autoplay: true,
+    slidesToScroll: 2,
+    autoplaySpeed: 1000000,
+    arrows: false,
+    dots: true,
+    dotsClass: 'dots_design'
+  });
+});
+// slick slider js ends here
+// typed js starts 
+var typed = new Typed('#element', {
+      strings: ['high-converting', 'modern websites'],
+      typeSpeed: 150,
+    });
+// typed js ends
+// sweetalert starts here
+
+Swal.fire({
+  title: 'Page Loading Successfull',
+  icon: 'success',
+  timer: 10000,
+  timerProgressBar: true,
+  showConfirmButton: true,
+});
+// sweetalert ends here
